c2f4b7a82e897eaec24d1e145939db7e7746fdb5
Marco <kuvimarco@gmail.com>
2020-08-27 15:29:23 +0200

    Add TellMeSomethingAboutMe Class

---
+++ src/TellMeSomethingAboutMe.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class TellMeSomethingAboutMe {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("How is your forname?");
+        String forname = scan.next();
+        System.out.println("And your surname?");
+        String surname = scan.next();
+        System.out.println("How old are you?");
+        int age = scan.nextInt();
+        System.out.println("Are you married?");
+        boolean status = scan.nextBoolean();
+        System.out.println("How tall are you?");
+        double height = scan.nextDouble();
+
+    }
+}
